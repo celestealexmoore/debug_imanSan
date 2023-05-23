0f7d214e556dc032f590a0943535dcced72ef03d
Celeste A. Moore <77650238+celestealexmoore@users.noreply.github.com>
2023-05-22 20:24:38 -0500

    working

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # debug_imanSan
 
-1. multipleWords shouldAlways be *camelCased.* thisIsCamelCase
+1. multipleWords shouldAlways be *camelCased.* thisIsCamelCase  
 ^ weatherapiurl -> weatherApiUrl, searchHistory, currentDay, etc.
 
-2. Check for spelling on variables (Download the VSCode extension "Code Spell Checker")
+2. Check for spelling on variables (Download the VSCode extension "Code Spell Checker")  
 ^ Switched forcast to forecast
 
 3. Pseudocoding is the technique you plan to use to solve a problem with code. (Line 1)
 
 4. Repo files should be neatly organized, ideally with a separation of concerns. (See assets folder structure).
+
+5. HTML file is missing a couple </div> tags.
+
+6. Random </link> on line 7 in HTML.
+
+7. You need to either add the CDN for bootstrap icons in order to load icons. Adding bootstrap and bootstrap icons are two different downloads. Since we are not using Node to install Bootstrap icons, you will navigate to this page and copy/paste the svg inside the button tag.  
+https://icons.getbootstrap.com/icons/search/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,85 @@
 // //psudocoding global variables
-var weatherApiUrl = "http://api.openweathermap.org/"
-var apiKey = '7821430c7057a531aa96447991e46bd9'
+var weatherApiUrl = "http://api.openweathermap.org/";
+var apiKey = "7821430c7057a531aa96447991e46bd9";
 
 //html GLOBAL variables
-var searchBtn = document.getElementById("search-btn")
-var search = document.getElementById("search")
-var searchHistory = document.getElementById("search-history")
-var today = document.getElementById("today")
-var currentDay = document.getElementById("current-day")
-var forecast = document.getElementById("forecast")
+var searchBtn = document.getElementById("search-btn");
+var search = document.getElementById("search");
+var searchHistory = document.getElementById("search-history");
+var today = document.getElementById("today");
+var currentDay = document.getElementById("current-day");
+var forecast = document.getElementById("forecast");
 
 ///geo/1.0/direct?q=London&limit=5&appid={API key}
 //data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-searchBtn.addEventListener("click", searchval)
-function searchval(event){
-    if (!search.value){
-        return
-    }
-    event.preventDefault()
-    var searchform = search.value.trim()
-    fetchcity(searchform)
+searchBtn.addEventListener("click", searchVal);
+function searchVal(event) {
+  if (!search.value) {
+    return;
+  }
+  event.preventDefault();
+  var searchForm = search.value.trim();
+  fetchCity(searchForm);
 }
 
-function fetchcity(searchform){
-   var apiurl =`${weatherApiUrl}geo/1.0/direct?q=${searchform}&limit=5&appid=${apiKey}`
-   console.log(apiurl)
-    fetch(apiurl)
+function fetchCity(searchForm) {
+  var apiUrl = `${weatherApiUrl}geo/1.0/direct?q=${searchForm}&limit=5&appid=${apiKey}`;
+  console.log(apiUrl);
+  fetch(apiUrl)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-    console.log(data[0])
-    console.log(data[0].name)
-    //savedcity (searchform)
-    fetchweather(data[0])
+      console.log(data[0]);
+      console.log(data[0].name);
+      //savedcity (searchForm)
+      fetchWeather(data[0]);
     })
     .catch(function (err) {
       console.error(err);
     });
 }
-function fetchweather (location){
-    var{lat,lon} = location
-    var city = location.name
-    var apiurl =`${weatherApiUrl}data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
-   console.log(apiurl)
-    fetch(apiurl)
+function fetchWeather(location) {
+  var { lat, lon } = location;
+  var city = location.name;
+  var apiUrl = `${weatherApiUrl}data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  console.log(apiUrl);
+  fetch(apiUrl)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-    console.log(data)
-    //console.log(data[0].name)
-    displayweather(city, data)
+      console.log(data);
+      //console.log(data[0].name)
+      displayWeather(city, data);
     })
     .catch(function (err) {
       console.error(err);
     });
 }
 
-function displayweather (city, data){
-    currentweather(city, data.list[0], data.city.timezone )
-    fivedayforcast(data.list)
+function displayWeather(city, data) {
+  currentWeather(city, data.list[0], data.city.timezone);
+  fiveDayForecast(data.list);
 }
 
-function currentweather (city, weather, timezone){
-    console.log(weather)
-    console.log (timezone)
-    var citydisplay = document.querySelector(".cardTodayCityName")
-    citydisplay.textContent = city
+function currentWeather(city, weather, timezone) {
+  console.log(weather);
+  console.log(timezone);
+  var citydisplay = document.querySelector(".cardTodayCityName");
+  citydisplay.textContent = city;
 
-    var humidity = weather.main.humidity
-    var temp = weather.main.temp
-    var windspeed = weather.wind.speed
- var humidityel = document.createElement("p")
- humidityel.setAttribute('class', 'card-text');
-    var datadisplay = document.querySelector(".cardBodyToday")
-    humidityel.textContent = `humidity: ${humidity}`
-datadisplay.appendChild(humidityel)
+  var humidity = weather.main.humidity;
+  var temp = weather.main.temp;
+  var windSpeed = weather.wind.speed;
+  var humidityEl = document.createElement("p");
+  humidityEl.setAttribute("class", "card-text");
+  var datadisplay = document.querySelector(".cardBodyToday");
+  humidityEl.textContent = `humidity: ${humidity}`;
+  datadisplay.appendChild(humidityEl);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-function fivedayforcast (forecast){
-    console.log(forecast)
-
-
-
-
-
-
-
-
-
-
-
+function fiveDayForecast(forecast) {
+  console.log(forecast);
 }
